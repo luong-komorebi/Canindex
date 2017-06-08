95f5cdc1ab162ec67c9f6f3e1e9da18c8a961528
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-06-08 19:56:14 +0700

    First commit. Back it up

---
+++ app/src/main/java/luongvo/com/imageuploader/DogInfo.java
@@ -0,0 +1,26 @@
+package luongvo.com.imageuploader;
+
+/**
+ * Created by luongvo on 07/06/2017.
+ */
+
+public class DogInfo {
+    private String title;
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
---
+++ app/src/main/java/luongvo/com/imageuploader/Service.java
@@ -0,0 +1,20 @@
+package luongvo.com.imageuploader;
+
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+/**
+ * Created by luongvo on 05/06/2017.
+ */
+
+interface Service {
+    @Multipart
+    @POST("/")
+    Call<ResponseBody> postImage(@Part MultipartBody.Part image, @Part("name")RequestBody name);
+}
